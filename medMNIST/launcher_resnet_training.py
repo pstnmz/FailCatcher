a01afe9a218d139c6f5cd332a642cddc5e711d54
psteinmetzforgood <pst.stein@gmail.com>
2025-10-30 14:16:04 +0100

    rebuild correct medMNIST arguments lists

--- medMNIST/launcher_resnet_training.py
+++ medMNIST/launcher_resnet_training.py
@@ -1,10 +1,10 @@
 import subprocess, shlex
 
-flags = ['dermamnist', 'octmnist', 'pathmnist', 'bloodmnist', 'dermamnist-e', 'breastmnist', 'organamnist', 'pneumoniamnist', 'dermamnist', 'octmnist', 'pathmnist', 'bloodmnist', 'tissuemnist', 'dermamnist-e']
+flags = ['tissuemnist', 'dermamnist', 'octmnist', 'pathmnist', 'bloodmnist', 'dermamnist-e', 'breastmnist', 'organamnist', 'pneumoniamnist', 'dermamnist', 'octmnist', 'pathmnist', 'bloodmnist', 'tissuemnist', 'dermamnist-e']
 colors = [False, True, False, True, True, True, False, False, False, True, False, True, True, False, True]  # Colors for the flags
 #batch_sizes = [32, 640, 128, 128, 640, 640, 640, 640, 128]  # Batch sizes for the flags
 
-use_randaugments = [False, False, False, False, False, True, True, True, True, True, True, True, True, True]         # <- enable/disable RandAugment here
+use_randaugments = [False, False, False, False, False, False, True, True, True, True, True, True, True, True, True]         # <- enable/disable RandAugment here
 num_epochs = 100
 batch_size = 128
 python = "/home/psteinmetz/venvs/venv_medMNIST/bin/python3.12"  # or path to your venv python
