dd0cb0f335000b23c1412ad9a207e95500e85e15
psteinmetzforgood <pst.stein@gmail.com>
2025-10-31 14:41:45 +0100

    Add early stopping criterion to resnet trainer + add class_weight parameter to cross entropy loss

--- medMNIST/launcher_resnet_training.py
+++ medMNIST/launcher_resnet_training.py
@@ -1,12 +1,13 @@
 import subprocess, shlex
 
-flags = ['tissuemnist', 'dermamnist', 'octmnist', 'pathmnist', 'bloodmnist', 'dermamnist-e', 'breastmnist', 'organamnist', 'pneumoniamnist', 'dermamnist', 'octmnist', 'pathmnist', 'bloodmnist', 'tissuemnist', 'dermamnist-e']
-colors = [False, True, False, True, True, True, False, False, False, True, False, True, True, False, True]  # Colors for the flags
+flags = ['breastmnist', 'organamnist', 'pneumoniamnist', 'dermamnist', 'octmnist', 'pathmnist', 'bloodmnist', 'tissuemnist', 'dermamnist-e', 'breastmnist', 'organamnist', 'pneumoniamnist', 'dermamnist', 'octmnist', 'pathmnist', 'bloodmnist', 'tissuemnist', 'dermamnist-e']
+colors = [False, False, False, True, False, True, True, False, True, False, False, False, True, False, True, True, False, True]  # Colors for the flags
 #batch_sizes = [32, 640, 128, 128, 640, 640, 640, 640, 128]  # Batch sizes for the flags
 
-use_randaugments = [False, False, False, False, False, False, True, True, True, True, True, True, True, True, True]         # <- enable/disable RandAugment here
+use_randaugments = [False, False, False, False, False, False, False, False, False, True, True, True, True, True, True, True, True, True]         # <- enable/disable RandAugment here
 num_epochs = 100
 batch_size = 128
+
 python = "/home/psteinmetz/venvs/venv_medMNIST/bin/python3.12"  # or path to your venv python
 
 for f, c, r in zip(flags, colors, use_randaugments) :
